6197db4d309c862353dcba1086a410d860846553
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-24 11:13:45 +0400

    Add solution for Logic1-cigarParty

---
+++ Logic1/cigarParty.java
@@ -0,0 +1,13 @@
+package Logic1;
+
+public class cigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(isWeekend && cigars > 60){
+            return true;
+        }else if(cigars >= 40 && cigars <=60){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}

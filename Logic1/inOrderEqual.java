ce37f38011e16a74f26fb078d3a6a20ae315224a
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-27 09:00:22 +0400

    Add solution for Logic1-inOrderEqual

---
+++ Logic1/inOrderEqual.java
@@ -0,0 +1,14 @@
+package Logic1;
+
+public class inOrderEqual {
+    public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+        if (!equalOk && a < b && b < c){
+            return true;
+        }
+        if (equalOk && a <= b && b <= c){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}

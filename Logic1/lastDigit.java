475f82dd12160304cc9506293de0344887349464
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-27 09:00:53 +0400

    Add solution for Logic1-lastDigit

---
+++ Logic1/lastDigit.java
@@ -0,0 +1,15 @@
+package Logic1;
+
+public class lastDigit {
+    public boolean lastDigit(int a, int b, int c) {
+        int modA = a % 10;
+        int modB = b % 10;
+        int modC = c % 10;
+
+        if (modA == modB || modA == modC || modB == modC){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

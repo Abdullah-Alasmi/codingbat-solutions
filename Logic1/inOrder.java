aa57961e81dc6b67503b0de577727f638123b221
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-27 08:59:48 +0400

    Add solution for Logic1-inOrder

---
+++ Logic1/inOrder.java
@@ -0,0 +1,18 @@
+package Logic1;
+
+public class inOrder {
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+        if (bOk){
+            if (c > b){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        if (b > a && c > b){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}

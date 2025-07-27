c6f0f42758affde48c8b1e01c25c1b5d2cf0b960
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-27 08:56:49 +0400

    Add solution for Logic1-answerCell

---
+++ Logic1/answerCell.java
@@ -0,0 +1,14 @@
+package Logic1;
+
+public class answerCell {
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if(isAsleep){
+            return false;
+        }
+        if ((isMorning && isMom) || !isMorning){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}

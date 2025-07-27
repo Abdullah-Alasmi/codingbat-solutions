c5383e1ee1fa20ddce656f5ecc828e65d8dc6b9a
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-27 08:58:44 +0400

    Add solution for Logic1-fizzString2

---
+++ Logic1/fizzString2.java
@@ -0,0 +1,16 @@
+package Logic1;
+
+public class fizzString2 {
+    public String fizzString2(int n) {
+        boolean fizz = n % 3 == 0;
+        boolean buzz = n % 5 == 0;
+
+        if (fizz && buzz)
+            return "FizzBuzz!";
+        if (fizz)
+            return "Fizz!";
+        if (buzz)
+            return "Buzz!";
+        return n + "!";
+    }
+}

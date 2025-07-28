0bfd6f8fa0642c05b0ccda5bdcd7f8b8be682647
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 01:08:05 +0400

    Add solution for String2-starOut

---
+++ String2/starOut.java
@@ -0,0 +1,7 @@
+public class starOut {
+    public String starOut(String str) {
+        String pattern = "([a-zA-Z0-9]{0,1})\\*+([a-zA-Z0-9]{0,1})";
+        str = str.replaceAll(pattern, "");
+        return str;
+    }
+}

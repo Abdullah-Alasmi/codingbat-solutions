73bcb541f8b5bdc22f96e13b6d585366dee956a3
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 17:14:50 +0400

    Add solution for String1-endsLy

---
+++ String-1/endsLy.java
@@ -0,0 +1,12 @@
+public class endsLy {
+    public boolean endsLy(String str) {
+        int length = str.length();
+        if(length <2) {
+            return false;
+        } else if(str.substring(length-2, length).equals("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

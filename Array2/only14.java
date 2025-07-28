4d243f211be34319570e49de128c7859547f5212
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 00:21:15 +0400

    Add solution for Array2-only14

---
+++ Array2/only14.java
@@ -0,0 +1,16 @@
+public class only14 {
+    public boolean only14(int[] nums) {
+        int count = 0;
+        for(int element: nums) {
+            if(element != 4 && element != 1) {
+                count++;
+            }
+        }
+        if(count > 0) {
+            return false;
+        } else {
+            return true;
+
+        }
+    }
+}

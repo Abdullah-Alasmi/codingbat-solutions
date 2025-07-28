e68d63d006fb4e5e3a3b5ca3afc64513e29e9957
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 17:13:46 +0400

    Add solution for String1-middleTwo

---
+++ String-1/middleTwo.java
@@ -0,0 +1,10 @@
+public class middleTwo {
+    public String middleTwo(String str) {
+        int length = str.length();
+        if(length == 2) {
+            return str;
+        } else {
+            return str.substring((length/2) - 1, (length/2) + 1);
+        }
+    }
+}

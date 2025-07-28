45257054cfa89c98a0437c3c94f4ccdccab661f2
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 20:18:51 +0400

    Add solution for Map2-wordCount

---
+++ Map2/wordCount.java
@@ -0,0 +1,14 @@
+public class wordCount {
+    public Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        for (String s:strings) {
+            String tmp = s;
+            if (map.containsKey(tmp)) {
+                int count = map.get(tmp);
+                map.put(tmp, count + 1);
+            } else
+                map.put(tmp, 1);
+        }
+        return map;
+    }
+}

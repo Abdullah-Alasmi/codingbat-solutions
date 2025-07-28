ec2f2cafad8d9ac857244c9de82d22fb817ccb8b
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 20:23:59 +0400

    Add solution for Map2-allSwap mod

--- Map2/allSwap.java
+++ Map2/allSwap.java
@@ -1,13 +1,15 @@
 public class allSwap {
-    Map<String, Integer> map = new HashMap();
-  for (int i = 0; i < strings.length; i++){
-        if (map.containsKey(strings[i].charAt(0) + "")){
-            String string = strings[i];
-            strings[i] = strings[map.get(strings[i].charAt(0) + "")];
-            strings[map.get(strings[i].charAt(0) + "")] = string;
-            map.remove(strings[i].charAt(0) + "");
-        } else
-            map.put(strings[i].charAt(0) + "", i);
+    public String[] allSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        for (int i = 0; i < strings.length; i++){
+            if (map.containsKey(strings[i].charAt(0) + "")){
+                String string = strings[i];
+                strings[i] = strings[map.get(strings[i].charAt(0) + "")];
+                strings[map.get(strings[i].charAt(0) + "")] = string;
+                map.remove(strings[i].charAt(0) + "");
+            } else
+                map.put(strings[i].charAt(0) + "", i);
+        }
+        return strings;
     }
-  return strings;
 }
--- Map2/wordAppend.java
+++ Map2/wordAppend.java
@@ -1,18 +1,20 @@
 public class wordAppend {
-    Map<String, Integer> map = new HashMap();
-    String string = "";
+    public String wordAppend(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        String string = "";
 
-  for(String s: strings) {
-        String key = s;
+        for(String s: strings) {
+            String key = s;
 
-        if (map.containsKey(key)) {
-            int value = map.get(key);
-            value++;
-            if (value % 2 == 0)
-                string += key;
-            map.put(key, value);
-        } else
-            map.put(key, 1);
+            if (map.containsKey(key)) {
+                int value = map.get(key);
+                value++;
+                if (value % 2 == 0)
+                    string += key;
+                map.put(key, value);
+            } else
+                map.put(key, 1);
+        }
+        return string;
     }
-  return string;
 }

1a412f02b7e1765870fdb17da0b929abad6aa003
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 20:16:56 +0400

    Add solution for Map2-wordLen

---
+++ Map2/wordLen.java
@@ -0,0 +1,9 @@
+public class wordLen {
+    public Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        for (String s:strings) {
+            map.put(s, s.length());
+        }
+        return map;
+    }
+}

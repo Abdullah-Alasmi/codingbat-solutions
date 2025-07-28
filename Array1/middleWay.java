43ade4c9bf914321319df6828253fd392c4bd4ae
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 18:36:40 +0400

    Add solution for Array1-middleWay

---
+++ Array1/middleWay.java
@@ -0,0 +1,8 @@
+public class middleWay {
+    public int[] middleWay(int[] a, int[] b) {
+        int [] nums = new int[2];
+        nums[0] = a[1];
+        nums[1] = b[1];
+        return nums;
+    }
+}

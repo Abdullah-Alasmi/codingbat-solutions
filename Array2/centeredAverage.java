0a303590aa741b1a5abb5a1f36c042c29ed07bfe
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 00:10:17 +0400

    Add solution for Array2-centeredAverage

---
+++ Array2/centeredAverage.java
@@ -0,0 +1,18 @@
+public class centeredAverage {
+    int result = 0;
+    int largest = nums[0];
+    int smallest = nums[0];
+
+  for(int i: nums) {
+        result += i;
+
+        if(i > largest) {
+            largest = i;
+        }
+        if(i < smallest) {
+            smallest = i;
+        }
+    }
+    result -= (largest + smallest);
+  return result /= (nums.length - 2);
+}

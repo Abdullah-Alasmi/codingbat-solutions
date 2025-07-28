4e7992ba37611cad6072068e6b5ab1a2d71d7218
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 01:08:47 +0400

    Add solution for String2-plusOut

---
+++ String2/plusOut.java
@@ -0,0 +1,23 @@
+public class plusOut {
+    public String plusOut(String str, String word) {
+        String result = "";
+        int i=0;
+        int end = 0;
+        int l =str.length();
+        while(i < str.length()) {
+            end = i + word.length();
+            if( end > l) {
+                end=l;
+            }
+            if(str.substring(i,end).equals(word)) {
+                result = result + word;
+                i = i + word.length();
+            }
+            else {
+                result = result + "+";
+                i++;
+            }
+        }
+        return result;
+    }
+}

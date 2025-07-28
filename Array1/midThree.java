8e93393b391533796db5a10d6dfc985009d62507
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 19:34:20 +0400

    Add solution for Array1-midThree

---
+++ Array1/midThree.java
@@ -0,0 +1,9 @@
+public class midThree {
+    public int[] midThree(int[] nums) {
+        int [] newArray = {nums[(nums.length / 2) - 1], nums[(nums.length / 2)], nums[(nums.length / 2) + 1]};
+        if(nums.length == 3)
+            return nums;
+        else
+            return newArray;
+    }
+}

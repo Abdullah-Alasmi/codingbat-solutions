711fc72112f73be9d85e5430fff89cce5772193e
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 00:16:44 +0400

    Add solution for Array2-sum28

---
+++ Array2/sum28.java
@@ -0,0 +1,15 @@
+public class sum28 {
+    public boolean sum28(int[] nums) {
+        int twoCount = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 2) {
+                twoCount++;
+            }
+        }
+        if(twoCount * 2 == 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

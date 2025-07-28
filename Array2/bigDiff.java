5d0a79077f45e434232821de016030330744c4df
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 21:02:16 +0400

    Add solution for Array2-bigDiff

---
+++ Array2/bigDiff.java
@@ -0,0 +1,17 @@
+public class bigDiff {
+    public int bigDiff(int[] nums) {
+        int largest = 0;
+        for(int i: nums) {
+            if(i > largest) {
+                largest = i;
+            }
+        }
+        int smallest = largest;
+        for(int j: nums) {
+            if(j < smallest) {
+                smallest = j;
+            }
+        }
+        return largest - smallest;
+    }
+}

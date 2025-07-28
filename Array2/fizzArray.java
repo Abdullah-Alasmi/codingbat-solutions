90432f47946438b7a9c4940faadc21f3f3517a27
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-29 00:20:27 +0400

    Add solution for Array2-fizzArray

---
+++ Array2/fizzArray.java
@@ -0,0 +1,9 @@
+public class fizzArray {
+    public int[] fizzArray(int n) {
+        int [] array = new int [n];
+        for(int i = 0; i < n; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+}

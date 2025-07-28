4f062e8879c50637a8b01c3479d895bd07b144f4
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 15:46:50 +0400

    Add solution for String1-extraEnd

---
+++ String-1/extraEnd.java
@@ -0,0 +1,6 @@
+public class extraEnd {
+    public String extraEnd(String str) {
+        String lastTwo = str.substring(str.length() - 2, str.length());
+        return lastTwo + lastTwo + lastTwo;
+    }
+}

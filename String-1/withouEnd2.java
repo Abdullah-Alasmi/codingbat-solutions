af98b44c28496620ce9008e9726e4dd9a973e999
Abdullah-Alasmi <abdullah.s.alaasmi@gmail.com>
2025-07-28 15:59:25 +0400

    Add solution for String1-withouEnd2

---
+++ String-1/withouEnd2.java
@@ -0,0 +1,9 @@
+public class withouEnd2 {
+    public String withouEnd2(String str) {
+        if(str.length() == 0 || str.length() == 1) {
+            return "";
+        } else {
+            return str.substring(1, str.length() -1);
+        }
+    }
+}
